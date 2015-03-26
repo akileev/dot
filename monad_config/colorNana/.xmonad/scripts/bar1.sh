cbfb69b97b58219cb60531635de144142b12735d
Artem Kileev <kileev@artemkileev.ru>
2015-03-26 16:08:31 +0500

    Sytax fix

--- monad_config/colorNana/.xmonad/scripts/bar1.sh
+++ monad_config/colorNana/.xmonad/scripts/bar1.sh
@@ -7,4 +7,4 @@ screenHeight=$(xrandr -q | grep '*' | awk '{print $1}' | cut -d 'x' -f2)
 
 let width=$screenWidth-650
 
-dzen2 -p -ta l -e 'button3=' -fn 'Exo 2-8' -fg '$1' -bg '$2' -h $barHeight -w $width -y 0 -x 0
+dzen2 -p -ta l -e 'button3=' -fn 'Exo 2-8' -fg "$1" -bg "$2" -h $barHeight -w $width -y 0
